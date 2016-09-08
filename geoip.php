5fd24a7349f206846ba6bab7134cdcee26dcd56b
Pierre Rudloff <contact@rudloff.pro>
2016-09-09 01:17:21 +0200

    PHPCS

--- geoip.php
+++ geoip.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * Simple GeoIP API that can be used in place of Google Location Service in Firefox
- * 
+ *
  * PHP Version 5.4.4
- * 
+ *
  * @category API
  * @package  GeoIP
  * @author   Pierre Rudloff <contact@rudloff.pro>
@@ -12,7 +12,10 @@
  * */
 $record=geoip_record_by_name($_SERVER['REMOTE_ADDR']);
 echo json_encode(
-    array('location'=>array('lat'=>$record['latitude'],
-    'lng'=>$record['longitude']), 'accuracy'=>40000)
+    array(
+        'location'=>array(
+            'lat'=>$record['latitude'],
+            'lng'=>$record['longitude']
+        ), 'accuracy'=>40000
+    )
 );
-?>
