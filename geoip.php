8b4bcd4965fc53c7c9f5feaf23feba9ba109574a
Pierre Rudloff <contact@rudloff.pro>
2016-09-08 23:18:47 +0000

    Applied fixes from StyleCI

--- geoip.php
+++ geoip.php
@@ -1,21 +1,22 @@
 <?php
 /**
- * Simple GeoIP API that can be used in place of Google Location Service in Firefox
+ * Simple GeoIP API that can be used in place of Google Location Service in Firefox.
  *
  * PHP Version 5.4.4
  *
  * @category API
- * @package  GeoIP
+ *
  * @author   Pierre Rudloff <contact@rudloff.pro>
  * @license  LGPL https://www.gnu.org/licenses/lgpl.html
+ *
  * @link     https://rudloff.pro/divers/geoip/
  * */
-$record=geoip_record_by_name($_SERVER['REMOTE_ADDR']);
+$record = geoip_record_by_name($_SERVER['REMOTE_ADDR']);
 echo json_encode(
-    array(
-        'location'=>array(
-            'lat'=>$record['latitude'],
-            'lng'=>$record['longitude']
-        ), 'accuracy'=>40000
-    )
+    [
+        'location' => [
+            'lat' => $record['latitude'],
+            'lng' => $record['longitude'],
+        ], 'accuracy' => 40000,
+    ]
 );
